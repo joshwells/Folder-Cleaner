69f35f4a27016698a765b84e5ef9fd015842af61
Joshua Wells <joshua.wells@gtri.gatech.edu>
2025-04-13 13:01:38 -0400

    Initial commit

---
+++ Cleanup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+date
+echo
+
+# Remove all files older than X days
+# ls -l /Users/jwells30/Downloads
+echo Removing the following old files:
+find $1 -type f -mtime +$2 -print -delete
+echo
+
+# Remove any empty folders older than X days
+echo Removing the following old, empty folders:
+find $1 -type d -mtime +$2 -empty -mindepth 1 -print -delete
+echo
+
+echo Processing complete
+echo
+echo
